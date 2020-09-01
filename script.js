7a07502793dc6dfcb7d4a964652282e01390997c
douglasjordan2 <douglasjordan2@gmail.com>
2020-09-01 00:32:44 -0600

    used js to populate number and operation buttons

---
+++ script.js
@@ -0,0 +1,32 @@
+// populate number buttons
+Array.from(Array(11)).forEach((a, i) => document.querySelector('#numbers').innerHTML += `<div class="btns" style="grid-area: ${i === 10 ? 'clr' : '_' + i};" id=${i === 10 ? 'clr' : '_' + i}>${i === 10 ? 'C' : i}</div>`)
+
+// populate operation buttons
+for(let i = 0; i < 5; i++) {
+  let op;
+  let btn;
+  switch(i) {
+    case 0:
+      op = 'equals';
+      btn="=";
+      break;
+    case 1:
+      op = 'plus';
+      btn="+";
+      break;
+    case 2:
+      op = 'minus';
+      btn = "-";
+      break;
+    case 3:
+      op = 'times';
+      btn = "x"
+      break;
+    case 4:
+      op = 'divide';
+      btn="&divide;"
+      break;
+  }
+
+  document.querySelector('#operations').innerHTML += `<div class="btns" style="grid-area: ${op};" id="${op}">${btn}</div>`
+}
